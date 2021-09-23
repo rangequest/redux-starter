2b9fff620257d7c49a93fdd5da8bba0c9162115d
Range Quest <range.quest@gmail.com>
2021-09-23 17:08:52 +0530

    ♿️ Refactored code according to dux pattern.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux'
-import reducer from './reducer'
+import reducer from './bugs'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 
 const store = createStore(reducer, devToolsEnhancer({ trace: true }))
-export default store
+
+export default function configureStore() {
+  return store
+}
