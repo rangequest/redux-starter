241a9ae3e9c723f47c67a5568f5dc1851f513f38
Range Quest <range.quest@gmail.com>
2021-09-23 18:32:08 +0530

    🚧 Projects slice in place.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import configureStore from './store/configureStore'
 import * as actions from './store/bugs'
+import { projectAdded, projectCompleted } from './store/projects'
 
 const store = configureStore()
 
-// store.subscribe(() => {
-//   console.log('Store changed: ', store.getState())
-// })
+store.subscribe(() => {
+  console.log('Store changed: ', store.getState())
+})
+
+store.dispatch(projectAdded({ name: 'Project1' }))
+store.dispatch(projectAdded({ name: 'Project2' }))
+store.dispatch(projectAdded({ name: 'Project3' }))
+
+store.dispatch(projectCompleted({ id: 1 }))
 
 store.dispatch(actions.bugAdded({ description: 'bug1' }))
 store.dispatch(actions.bugAdded({ description: 'bug2' }))
