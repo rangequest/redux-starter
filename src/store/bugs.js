855532f28f8e7365ce529cc9c2b9cff326d30c02
Range Quest <range.quest@gmail.com>
2021-09-23 18:11:45 +0530

    🏗️ Made life easy with createSlice function.

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,26 +1,24 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
-
-// Action creators
-export const bugAdded = createAction('bugAdded')
-export const bugRemoved = createAction('bugRemoved')
-export const bugResolved = createAction('bugResolved')
-
-// Reducer
+import { createSlice } from '@reduxjs/toolkit'
 let lastId = 0
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    })
-  },
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex(bug => bug.id === action.payload.id)
-    bugs[index].resolved = true
-  },
-  [bugRemoved.type]: (bugs, action) => {
-    bugs.map(bug => (bug.id === action.payload.id ? { ...bug, resolved: true } : bug))
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      })
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id)
+      bugs[index].resolved = true
+    },
   },
 })
+
+export default slice.reducer
+
+export const { bugAdded, bugResolved } = slice.actions
