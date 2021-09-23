980175d1fb65f7658e2578597e25d9c18828b8b3
Range Quest <range.quest@gmail.com>
2021-09-23 08:30:24 +0530

    Reducing code duplication with currying.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import { compose, pipe } from 'lodash/fp'
 // Normal programming
 let input = ' Range Quest   '
 let output = 'div' + input.trim(input) + '</div>'
-console.log('Normal: ', output)
+// console.log('Normal: ', output)
 
 // Function composition in Functional Programming
 // Think in functional blocks and compose them together to solve a problem
 const trimStr = str => str.trim()
 const wrapInDiv = str => `<div>${str}</div>`
+const wrap = type => str => `<${type}>${str}</${type}>`
+const toLowerCase = str => str.toLowerCase()
 
-output = wrapInDiv(trimStr(input))
-console.log('Function Composition: ', output)
+// output = wrapInDiv(trimStr(input))
+// console.log('Function Composition: ', output)
 
 // using loDash
-const transform = pipe(trimStr, wrapInDiv)
+const transform = pipe(trimStr, toLowerCase, wrap('span'))
 //const transform = compose(wrapInDiv, trimStr)
 output = transform(input)
 console.log('Using lodash: ', output)
