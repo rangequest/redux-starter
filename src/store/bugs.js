fcadb103de991be8660a56170d8a6c96d1cad97f
Range Quest <range.quest@gmail.com>
2021-09-23 22:27:11 +0530

    Memoizing with selector package.

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { createSelector } from 'reselect'
 let lastId = 0
 
 const slice = createSlice({
@@ -23,5 +24,7 @@ export default slice.reducer
 
 export const { bugAdded, bugResolved } = slice.actions
 
-// selector
-export const getUnresolvedBugs = state => state.entities.bugs.filter(bug => !bug.resolved)
+export const getUnresolvedBugs = createSelector(
+  state => state.entities.bugs,
+  bugs => bugs.filter(bug => !bug.resolved)
+)
