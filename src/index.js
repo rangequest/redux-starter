5f839b29e341573e4e24a2c76bee3d63705f8f9d
Range Quest <range.quest@gmail.com>
2021-09-23 16:32:33 +0530

    Redux devtools tracing with 'devToolsEnhancer'

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import store from './customStore'
-console.log(store)
+import store from './store'
+import * as actions from './actions'
+
+store.subscribe(() => {
+  console.log('Store changed: ', store.getState())
+})
+
+store.dispatch(actions.bugAdded('bug1'))
+store.dispatch(actions.bugAdded('bug2'))
+store.dispatch(actions.bugAdded('bug3'))
+
+store.dispatch(actions.bugResolved(1))
