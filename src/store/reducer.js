df42071341810d45014a17125a96fe11232b47d7
Range Quest <range.quest@gmail.com>
2021-09-23 22:05:59 +0530

    Introduced entities reducer.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
-import bugsReducer from './bugs'
-import projectsReducer from './projects'
+import entitiesReducer from './entities'
 
 export default combineReducers({
-  bugs: bugsReducer,
-  projects: projectsReducer,
+  entities: entitiesReducer,
 })
