dc429420a27aa1ce33e3b66ade816d0a913e5855
Range Quest <range.quest@gmail.com>
2021-09-24 13:20:29 +0530

    Simple jest tests for simple math module.

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,13 @@
-it('first test', () => {})
+import { isEven } from './math'
+
+describe('isEven', () => {
+  it('should return true for even number input', () => {
+    const result = isEven(2)
+    expect(result).toEqual(true)
+  })
+
+  it('should return false for odd number input', () => {
+    const result = isEven(1)
+    expect(result).toEqual(false)
+  })
+})
