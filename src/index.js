8799ad19eafc3cb8c9e848ccd1adf5d2b4c04d2d
Range Quest <range.quest@gmail.com>
2021-09-24 05:41:40 +0530

    🎨 Implemented a middleware for logging.

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ store.subscribe(() => {
 })
 
 store.dispatch(projectAdded({ name: 'Project1' }))
-store.dispatch(projectAdded({ name: 'Project2' }))
-store.dispatch(projectAdded({ name: 'Project3' }))
+// store.dispatch(projectAdded({ name: 'Project2' }))
+// store.dispatch(projectAdded({ name: 'Project3' }))
 
-store.dispatch(projectCompleted({ id: 1 }))
+// store.dispatch(projectCompleted({ id: 1 }))
 
-store.dispatch(bugAdded({ description: 'bug1' }))
-store.dispatch(bugAdded({ description: 'bug2' }))
-store.dispatch(bugAdded({ description: 'bug3' }))
+// store.dispatch(bugAdded({ description: 'bug1' }))
+// store.dispatch(bugAdded({ description: 'bug2' }))
+// store.dispatch(bugAdded({ description: 'bug3' }))
 
-store.dispatch(bugResolved({ id: 1 }))
+// store.dispatch(bugResolved({ id: 1 }))
 
 console.log(getUnresolvedBugs(store.getState()))
